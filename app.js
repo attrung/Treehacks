37fff0479e4921b7f3913ee596f030114ec7394e
atrung2901 <55957585+atrung2901@users.noreply.github.com>
2020-02-14 19:42:35 -0800

    Add files

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("landing");
+});
+
+app.get("/home", function(req, res){
+  res.redirect("/");
+});
+
+app.get("/main", function(req, res){
+  res.render("main");
+})
+
+app.listen(3000, '127.0.0.1', function(){
+  console.log("Server started!");
+})
