71d50835c323640e23c373389dec816b7ce4dea1
atrung2901 <55957585+atrung2901@users.noreply.github.com>
2020-02-14 22:30:49 -0800

    update front-end files

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", function(req, res){
   res.render("landing");
 });
@@ -17,6 +20,6 @@ app.get("/main", function(req, res){
   res.render("main");
 })
 
-app.listen(3000, '127.0.0.1', function(){
+app.listen(4000, '127.0.0.1', function(){
   console.log("Server started!");
 })
