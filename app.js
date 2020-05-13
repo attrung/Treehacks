ccf2468103e70d10a4a411942647a0f571712453
atrung2901 <55957585+atrung2901@users.noreply.github.com>
2020-05-13 16:28:54 +0700

    update for heroku

--- app.js
+++ app.js
@@ -7,15 +7,10 @@ const Houndify = require('houndify');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
   res.render("landing");
-});
-
-app.get("/home", function(req, res){
-  res.redirect("/");
 });
 
 app.get('/houndifyAuth', houndifyExpress.createAuthenticationHandler({
@@ -28,10 +23,6 @@ app.post('/textSearchProxy', houndifyExpress.createTextProxyHandler());
 
 app.get("/main", function(req, res){
   res.render("main");
-})
-
-app.get("/map", function(req, res){
-  res.render("main2")
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
